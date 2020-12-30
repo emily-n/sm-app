1c214005d4af68ba1d25d69b88f39c81f5dbfd51
Emilia Nenova <emilia.nenova1@gmail.com>
2020-12-30 16:06:33 +0200

    Add createPost with authentification

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt:-1 });
                 return posts;
             } catch (err) {
                 throw new Error(err);
@@ -22,5 +23,20 @@ module.exports = {
                 throw new Error(err);
             }
         }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            const user = checkAuth(context);
+            
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            });
+
+            const post = await newPost.save();
+            return post;
+        }
     }
-}
+};
