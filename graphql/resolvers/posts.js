5fb2782969abeffe5f0a2e57c567efe3cec607c1
Emilia Nenova <emilia.nenova1@gmail.com>
2020-12-30 16:20:29 +0200

    Add deletePost

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,11 +1,12 @@
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
+const { AuthenticationError } = require('apollo-server');
 
 module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find().sort({ createdAt:-1 });
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch (err) {
                 throw new Error(err);
@@ -27,7 +28,7 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
-            
+
             const newPost = new Post({
                 body,
                 user: user.id,
@@ -37,6 +38,22 @@ module.exports = {
 
             const post = await newPost.save();
             return post;
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+
+            try {
+                const post = await Post.findById(postId);
+                if (user.username === post.username) {
+                    await post.delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+
+                }
+            } catch (err) {
+                throw new Error(err); 
+            }
         }
     }
 };
