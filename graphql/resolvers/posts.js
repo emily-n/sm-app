305b07f946efaeaa6cd6a4d6bb43838cc37a1106
Emilia Nenova <emilia.nenova1@gmail.com>
2020-12-30 15:16:50 +0200

    Implement getPost query

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -3,10 +3,22 @@ const Post = require('../../models/Post');
 module.exports = {
     Query: {
         async getPosts() {
-            try{
+            try {
                 const posts = await Post.find();
                 return posts;
-            } catch(err) {
+            } catch (err) {
+                throw new Error(err);
+            }
+        },
+        async getPost(_, { postId }) {
+            try {
+                const post = await Post.findById(postId);
+                if (post) {
+                    return post;
+                } else {
+                    throw new Error('Post not found');
+                }
+            } catch (err) {
                 throw new Error(err);
             }
         }
