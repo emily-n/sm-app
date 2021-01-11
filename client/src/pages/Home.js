a2fce02ba0262099cd6625c3935d653c266977f4
Emilia Nenova <emilia.nenova1@gmail.com>
2021-01-11 17:25:26 +0200

    Display posts in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,58 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Grid, Segment} from 'semantic-ui-react';
+import PostCard from '../components/PostCard';
 
-function Homepage() {
+function Home() {
+    const {
+        loading,
+        data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+    console.log(posts);
+
     return (
-        <div>
-            <h1>
-                Home
-            </h1>
-        </div>
+        <Grid columns={1} padded>
+            <Grid.Row>
+                <h1 style={{paddingBottom: 20}}>
+                    Recent Posts
+                </h1>
+            </Grid.Row>
+            <Grid.Row>
+                {loading ? (
+                    <h1>Loading posts..</h1>
+                ) : (
+                        posts &&
+                        posts.map((post) => (
+                            <Grid.Column key={post.id} style={{ margin: 15, padding: 6}} color='grey'>
+                                <PostCard post={post}/>
+                            </Grid.Column>
+                        ))
+                    )}
+            </Grid.Row>
+        </Grid>
     )
 }
 
-export default Homepage; 
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts {
+            id
+            body
+            username
+            createdAt
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments{
+                id
+                username
+                createdAt
+                body
+            }
+        }
+    }
+`;
+
+export default Home; 
