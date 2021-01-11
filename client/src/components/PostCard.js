70336372215ac9fd5f8d61179933b61ec3e46c54
Emilia Nenova <emilia.nenova1@gmail.com>
2021-01-11 17:55:05 +0200

    Add like button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
     return (
@@ -12,11 +13,18 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
                     src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
                 />
                 <Card.Header>{username}</Card.Header>
-                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow()}</Card.Meta>
                 <Card.Description>{body}</Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <p>buttons here</p>
+                <Button as='div' labelPosition='right' size="mini">
+                    <Button color='red' basic>
+                        <Icon name='heart' />
+                    </Button>
+                    <Label basic color="pink" pointing='left'>
+                        {likeCount}
+                    </Label>
+                </Button>
             </Card.Content>
         </Card>
     );
