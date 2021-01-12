023e88bdd3bae97bb8f2e6bb6fd27abc836b7d62
Emilia Nenova <emilia.nenova1@gmail.com>
2021-01-12 14:35:27 +0200

    Add LikeButton

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -2,13 +2,11 @@ import React, { useContext } from 'react';
 import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import LikeButton from './LikeButton';
 import { AuthContext } from '../context/auth';
 
 function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes } }) {
     const { user } = useContext(AuthContext);
-    function likePost() {
-        console.log('Like post!');
-    }
 
     return (
         <Card fluid>
@@ -23,15 +21,8 @@ function PostCard({ post: { body, createdAt, id, username, likeCount, commentCou
                 <Card.Description>{body}</Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <Button as='div' labelPosition='right' size="mini" onClick={likePost}>
-                    <Button color='pink' basic>
-                        <Icon name='heart' />
-                    </Button>
-                    <Label basic color="pink" pointing='left'>
-                        {likeCount}
-                    </Label>
-                </Button>
-                <Button as='div' labelPosition='right' size="mini" as={Link} to={`/posts/${id}`}>
+                <LikeButton user={user} post={{ id, likes, likeCount }} />
+                <Button labelPosition='right' size="mini" as={Link} to={`/posts/${id}`}>
                     <Button color='blue' basic>
                         <Icon name='comments' />
                     </Button>
