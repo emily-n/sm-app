b2b855637e4eb66e374a3589f285e4361de9d2ab
Emilia Nenova <emilia.nenova1@gmail.com>
2021-01-12 12:41:34 +0200

    Get Posts from cache, handle error

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,29 +1,40 @@
-import { ValuesOfCorrectTypeRule } from 'graphql';
 import React from 'react'
 import { Form, Button } from 'semantic-ui-react';
 import { useForm } from '../util/hooks';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function PostForm() {
     const { values, onChange, onSubmit } = useForm(createPostCallback, {
         body: ''
     });
 
+
     const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
         variables: values,
-        update(_, result) {
-            console.log(result);
+        update(proxy, result) {
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY
+            });
+            proxy.writeQuery({ 
+                query: FETCH_POSTS_QUERY,
+                data: {
+                    getPosts: [result.data.createPost, ...data.getPosts]
+                } });
             values.body = '';
+        },
+        onError(err) {
+            return err;
         }
     });
-
 
     function createPostCallback() {
         createPost();
     }
 
     return (
+        <>
         <Form onSubmit={onSubmit}>
             <h2>Create a post:</h2>
             <Form.Field>
@@ -32,13 +43,21 @@ function PostForm() {
                     name="body"
                     onChange={onChange}
                     value={values.body}
+                    error={error ? true:false}
                 />
                 <Button type="submit" color="teal">
                     Submit
                 </Button>
             </Form.Field>
         </Form>
-
+        { error && (
+            <div className="ui error message" style={{ marginBottom: 20 }}>
+              <ul className="list">
+                <li>{error.graphQLErrors[0].message}</li>
+              </ul>
+            </div>
+        )}
+        </>
     );
 }
 
